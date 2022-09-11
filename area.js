c01c650c969baa6a87e325c40837a2ed769fe9de
mahimagupta24 <mahimagupta2401@gmail.com>
2022-09-11 11:01:53 +0530

    output message update

--- area.js
+++ area.js
@@ -4,7 +4,7 @@ const output = document.querySelector("#output");
 
 function checkArea() {
   if (Number(input[0].value) <=0||Number(input[1].value) <= 0) {
-    output.innerText = "The value should be greater than zero";
+    output.innerText = "The value should be greater than zero and it can't be empty";
   }else{
     const areaOfTriangle =
       (Number(input[0].value) * Number(input[1].value)) / 2;
