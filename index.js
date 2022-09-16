c176f609aff462a1dc382af213684d7cb25c7b60
mahimagupta24 <mahimagupta2401@gmail.com>
2022-09-16 12:11:45 +0530

    negative values fix

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const input=document.querySelectorAll(".angle-input")
 const showBtn=document.querySelector("#show-button")
 const output=document.querySelector("#output")
 showBtn.addEventListener("click",isTriangle)
-console.log(input)
+// console.log(input)
 
 function sumOfAngles(angle1,angle2,angle3){
     const sum=angle1+angle2+angle3
     return sum;
 }
 function isTriangle(){
+   
     const sum=sumOfAngles(Number(input[0].value),Number(input[1].value),Number(input[2].value))
-    if(sum ==""){
-        output.innerText="please enter the required input"
+
+    if(Number(input[0].value<0)||Number(input[1].value<0)||Number(input[2].value<0)){
+        output.innerText="Invalid angles"
+    }
+ else if(sum ==""){
+        output.innerText="Please insert the required input."
     }else if(sum===180){
     output.innerText="Yipee!its a triangle"
 }else{
